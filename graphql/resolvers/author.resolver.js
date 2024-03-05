104f6b5af9f8757e8d716e01bcb701ce9d899b60
shakilkhan12 <shakilkhan621@gmail.com>
2024-03-06 00:05:34 +0500

    resolved issue

--- graphql/resolvers/author.resolver.js
+++ graphql/resolvers/author.resolver.js
@@ -6,42 +6,40 @@ const AuthorModel = require("../../models/author.model");
 const authorResolver = {
   Mutation: {
     register: async (_, { inputArgs }) => {
-      const response = validateAllFields(inputArgs);
-      if (response.success) {
-        const { name, email, password } = inputArgs;
-        // try {
-        const emailExist = await AuthorModel.findOne({ email });
-        if (!emailExist) {
-          const salt = await bcrypt.genSalt(10);
-          const hashedPassword = await bcrypt.hash(password, salt);
-          await AuthorModel.create({ name, email, password: hashedPassword });
-          return {
-            success: true,
-            message: "Your accout has been created successfully",
-          };
+      try {
+        const response = validateAllFields(inputArgs);
+        if (response.success) {
+          const { name, email, password } = inputArgs;
+
+          const emailExist = await AuthorModel.findOne({ email });
+          if (!emailExist) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            await AuthorModel.create({ name, email, password: hashedPassword });
+            return {
+              success: true,
+              message: "Your accout has been created successfully",
+            };
+          } else {
+            throw new GraphQLError("Email is already taken", {
+              extensions: {
+                code: "BAD_REQUEST",
+                http: { status: 400 },
+              },
+            });
+          }
         } else {
-          throw new GraphQLError("Email is already taken", {
+          throw new GraphQLError(JSON.stringify(response.errors), {
             extensions: {
-              code: "BAD_REQUEST",
+              code: "BAD_USER_INPUT",
               http: { status: 400 },
             },
           });
         }
-        // } catch (error) {
-        //   throw new GraphQLError(error.message, {
-        //     extensions: {
-        //       code: "INTERNAL_SERVER_ERROR",
-        //       http: { status: 500 },
-        //     },
-        //   });
-        // }
-      } else {
-        console.log("error");
-        throw new GraphQLError(JSON.stringify(response.errors), {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            http: { status: 400 },
-          },
+      } catch (error) {
+        console.log("error ==> ", error.extensions);
+        throw new GraphQLError(error.message, {
+          extensions: { ...error.extensions },
         });
       }
     },
